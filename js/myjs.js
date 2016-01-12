cd5b4a3fd0e2e4e192cfe4b7f3b323c04c3b4e31
Alex Fallenstedt and Chris Kuiper <chris.kuiper@gmail.com>
2016-01-11 16:39:47 -0800

    add script to terms for displaying definitions

---
+++ js/myjs.js
@@ -0,0 +1,59 @@
+$(function() {
+  $(".clickable").click(function() {
+    $(".value-fadetoggle").fadeToggle();
+  });
+  $(".clickable2").click(function() {
+    $(".value-fadetoggle2").fadeToggle();
+  });
+  $(".clickable3").click(function() {
+    $(".value-fadetoggle3").fadeToggle();
+  });
+  $(".clickable4").click(function() {
+    $(".value-fadetoggle4").fadeToggle();
+  });
+  $(".clickable5").click(function() {
+    $(".value-fadetoggle5").fadeToggle();
+  });
+  $(".clickable6").click(function() {
+    $(".value-fadetoggle6").fadeToggle();
+  });
+  $(".clickable7").click(function() {
+    $(".value-fadetoggle7").fadeToggle();
+  });
+  $(".clickable8").click(function() {
+    $(".value-fadetoggle8").fadeToggle();
+  });
+  $(".clickable9").click(function() {
+    $(".value-fadetoggle9").fadeToggle();
+  });
+  $(".clickable10").click(function() {
+    $(".value-fadetoggle10").fadeToggle();
+  });
+  $(".clickable11").click(function() {
+    $(".value-fadetoggle11").fadeToggle();
+  });
+  $(".clickable12").click(function() {
+    $(".value-fadetoggle12").fadeToggle();
+  });
+  $(".clickable13").click(function() {
+    $(".value-fadetoggle13").fadeToggle();
+  });
+  $(".clickable14").click(function() {
+    $(".value-fadetoggle14").fadeToggle();
+  });
+  $(".clickable15").click(function() {
+    $(".value-fadetoggle15").fadeToggle();
+  });
+  $(".clickable16").click(function() {
+    $(".value-fadetoggle16").fadeToggle();
+  });
+  $(".clickable17").click(function() {
+    $(".value-fadetoggle17").fadeToggle();
+  });
+  $(".clickable18").click(function() {
+    $(".value-fadetoggle18").fadeToggle();
+  });
+
+
+
+});
